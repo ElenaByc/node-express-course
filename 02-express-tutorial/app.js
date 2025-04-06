aca84fae52a1495b0c7c9eb86e41cc236ec093ca
Elena Bychenkova <elena.bychenkova@gmail.com>
2025-04-05 19:09:41 -0700

    add logger, get and post for people

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,40 @@
 const express = require('express')
-const { products } = require('./data')
+const { products, people } = require('./data')
 
 const app = express()
 const port = 3000
 
-app.use(express.static('./public'))
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const time = new Date().getFullYear()
+  console.log(method, url, time)
+  next()
+}
 
+
+app.use(express.static('./methods-public'))
+app.use(logger)
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.get('/api/v1/test', (req, res) => {
-  res.json({ message: "It worked!" })
+  res.json({ message: 'It worked!' })
+})
+
+app.get('/api/v1/people/', (req, res) => {
+  res.json(people)
+})
+
+app.post('/api/v1/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please provide a name' })
+  }
+  people.push({ id: people.length + 1, name: name })
+  res.status(201).json({ success: true, name: name })
 })
 
 app.get('/api/v1/products/', (req, res) => {
@@ -39,7 +65,7 @@ app.get('/api/v1/query', (req, res) => {
     )
   }
   if (price && Number(price) >= 0) {
-    filteredProducts = filteredProducts.filter(product => 
+    filteredProducts = filteredProducts.filter(product =>
       product.price <= Number(price)
     )
   }
