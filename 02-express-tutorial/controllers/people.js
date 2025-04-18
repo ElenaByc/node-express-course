37a4c4dd5b4cf28994f43f3b24aef5d5fc8fca05
Elena Bychenkova <elena.bychenkova@gmail.com>
2025-04-17 21:56:01 -0700

    fix edding and deleting a person

--- 02-express-tutorial/controllers/people.js
+++ 02-express-tutorial/controllers/people.js
@@ -1,5 +1,13 @@
 const { people } = require('../data')
 
+let lastUsedId = -1
+
+people.forEach((person) => {
+  if (person.id > lastUsedId) {
+    lastUsedId = person.id  
+  }
+})
+
 const getPeople = (req, res) => {
   res.status(200).json({ success: true, data: people })
 }
@@ -11,7 +19,8 @@ const addPerson = (req, res) => {
       .status(400)
       .json({ success: false, message: 'Please provide a name' })
   }
-  people.push({ id: people.length + 1, name })
+  lastUsedId++
+  people.push({ id: lastUsedId, name })
   res.status(201).json({ success: true, name })
 }
 
@@ -26,14 +35,13 @@ const updatePerson = (req, res) => {
       .status(404)
       .json({ success: false, msg: `no person with id ${id}` })
   }
-  const updatedPeople = people.map((person) => {
+  people.map((person) => {
     if (person.id === Number(id)) {
       person.name = name
     }
-    return person
   })
 
-  res.status(200).json({ success: true, data: updatedPeople })
+  res.status(200).json({ success: true, data: people })
 }
 
 const deletePerson = (req, res) => {
@@ -46,7 +54,13 @@ const deletePerson = (req, res) => {
   const updatedPeople = people.filter(
     (person) => person.id !== Number(req.params.id)
   )
-  return res.status(200).json({ success: true, data: updatedPeople })
+
+  people.length = 0
+  updatedPeople.forEach((person) => {
+    people.push(person)
+  })
+
+  return res.status(200).json({ success: true, data: people })
 }
 
 
